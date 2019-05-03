432547706b002a8686cc8850fa7dfba7b89f768a
Garrett Klatte <gklatte@ucla.edu>
2019-05-03 03:44:31 +0000

    Make Index ready

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,29 +1,16 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-//import './index.css';
-//import App from './App';
-//import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
-import graphIsomorphism from './reducers'
-import { setPosition } from './actions'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import graphIsomorphism from './reducers';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+const store = createStore(graphIsomorphism);
 
-const store = createStore(graphIsomorphism)
-
-
-
-console.log(store.getState())
-
-const unsubscribe = store.subscribe(() => console.log(store.getState()))
-
-store.dispatch(setPosition('A', 1, 13))
-store.dispatch(setPosition('A', 1, 14))
-store.dispatch(setPosition('A', 1, 15))
-
-unsubscribe()
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
