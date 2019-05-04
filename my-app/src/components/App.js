4226c1ed1b796e9a3e3489c89299edc875571bd9
Garrett Klatte <gklatte@ucla.edu>
2019-05-04 18:31:07 +0000

    Fix background color

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -5,19 +5,19 @@ import TargetGraph from '../containers/TargetGraph';
 
 const App = () => (
   <div className="App">
-    <div className="Container">
-      <nav>
-        <ul className="Nav">
-          <li><p>Graph Isomorphism</p></li>
-          <li><p>Easy</p></li>
-          <li><p>Medium</p></li>
-          <li><p>Hard</p></li>
-        </ul>
-      </nav>
-      <div className="Graph-Body">
-        <TargetGraph />
-        <SourceGraph />
-      </div>
+    <nav>
+      <ul className="Nav">
+        <li><p>Graph Isomorphism</p></li>
+        <li><p>Easy</p></li>
+        <li><p>Medium</p></li>
+        <li><p>Hard</p></li>
+      </ul>
+    </nav>
+    <div className="Graph-Body">
+      <TargetGraph />
+      <SourceGraph />
+      <h2 className="Title">Target</h2>
+      <h2 className="Title">Source</h2>
     </div>
   </div>
 );
