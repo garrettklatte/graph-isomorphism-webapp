b0bed051df53fca9a13bcfce4b100bfa833d4043
Garrett Klatte <gklatte@ucla.edu>
2019-05-04 18:57:47 +0000

    Update dependency

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../App.css';
+import '../styles/App.css';
 import SourceGraph from '../containers/SourceGraph';
 import TargetGraph from '../containers/TargetGraph';
 
