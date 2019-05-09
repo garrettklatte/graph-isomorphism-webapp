caa7beeeb3d753e6eb63b526cbb326816151ed70
Garrett Klatte <gklatte@ucla.edu>
2019-05-09 00:34:48 +0000

    Test translation

--- my-app/src/services.js
+++ my-app/src/services.js
@@ -1,43 +1,7 @@
 import request from 'superagent';
-import uuidv4 from 'uuid/v4';
 
 import {EASY, MEDIUM, HARD, setGraph} from './actions';
-
-const translateVertices = (mapping, vertices) => (
-  vertices.map(vertex => mapping[vertex])
-)
-
-const translatePosition = (mapping, vertices, source) => (
-  vertices.reduce((accumulator, vertex) => (
-    {
-      ...accumulator,
-      [mapping[vertex]]: source[vertex]
-    }
-  ), {})
-)
-
-const translateEdges = (mapping, edges) => (
-  edges.map(edge => ({
-    start: mapping[edge.start],
-    end: mapping[edge.end],
-  }))
-)
-
-const translate = ({vertices, source, target, edges}) => {
-  const mapping = vertices.reduce((accumulator, vertex) => (
-    {
-      ...accumulator,
-      [vertex]: uuidv4()
-    }
-  ), {})
-
-  return {
-    vertices: translateVertices(mapping, vertices),
-    source: translatePosition(mapping, vertices, source),
-    target: translatePosition(mapping, vertices, target),
-    edges: translateEdges(mapping, edges)
-  }
-}
+import {translate} from './utils/translate'
 
 export const fetchEasyGraph = dispatch => () => {
   request
