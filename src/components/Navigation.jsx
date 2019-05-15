8870aa79af0c7dd31e96a8e14ecfe6b7c297b90d
Garrett Klatte <gklatte@ucla.edu>
2019-05-15 01:06:43 +0000

    All good on mobile

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,9 +4,14 @@ import styled from 'styled-components';
 import { EASY, MEDIUM, HARD } from '../actions';
 
 const Items = styled.ul`
+flex-direction: column;
+@media (min-width: 525px) {
+flex-direction: row;
+}
     display: flex;
     padding-left: 0;
     list-style-type: none;
+margin-top: 2em;
 `;
 
 const Name = styled.p`
@@ -20,6 +25,7 @@ const Name = styled.p`
     :hover {
         cursor: default;
     }
+margin: 0;
 `;
 
 const DifficultyName = styled(Name)`
@@ -27,6 +33,7 @@ const DifficultyName = styled(Name)`
       background-color: #a36b6c;
       cursor: pointer;
     }
+margin: 0;
 `;
 
 const ListItem = styled.li`
@@ -34,11 +41,35 @@ const ListItem = styled.li`
 `;
 
 const Title = styled(ListItem)`
+margin: 0 auto;
+@media (min-width: 525px) {
+margin-left: 0;
     margin-right: auto;
+}
 `;
 
 const Button = styled(ListItem)`
     margin-left: 1.5em;
+`;
+
+const Leader = styled(Button)`
+@media (max-width: 525px) {
+margin-left: auto;
+}
+`;
+
+const Loser = styled(Button)`
+@media (max-width: 525px) {
+margin-right: auto;
+}
+`;
+
+const Mini = styled.div`
+display: flex;
+margin-top: 1em;
+@media (min-width: 525px) {
+margin-top: 0;
+}
 `;
 
 const Navigation = ({
@@ -47,9 +78,11 @@ const Navigation = ({
   <nav>
     <Items>
       <Title><Name>Graph Isomorphism</Name></Title>
-      <Button onClick={onEasy(uri)}><DifficultyName chosen={difficulty === EASY}>Easy</DifficultyName></Button>
-      <Button onClick={onMedium(uri)}><DifficultyName chosen={difficulty === MEDIUM}>Medium</DifficultyName></Button>
-      <Button onClick={onHard(uri)}><DifficultyName chosen={difficulty === HARD}>Hard</DifficultyName></Button>
+      <Mini>
+        <Leader onClick={onEasy(uri)}><DifficultyName chosen={difficulty === EASY}>Easy</DifficultyName></Leader>
+        <Button onClick={onMedium(uri)}><DifficultyName chosen={difficulty === MEDIUM}>Medium</DifficultyName></Button>
+        <Loser onClick={onHard(uri)}><DifficultyName chosen={difficulty === HARD}>Hard</DifficultyName></Loser>
+      </Mini>
     </Items>
   </nav>
 );
