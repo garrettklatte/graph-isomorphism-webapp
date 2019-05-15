a49668e5b96b295a86f9952400cf537802d951bd
Garrett Klatte <gklatte@ucla.edu>
2019-05-14 21:47:42 -0400

    Improve layout on smaller screens (#21)

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,9 +4,14 @@ import styled from 'styled-components';
 import { EASY, MEDIUM, HARD } from '../actions';
 
 const Items = styled.ul`
+    flex-direction: column;
+    @media (min-width: 525px) {
+        flex-direction: row;
+    }
     display: flex;
     padding-left: 0;
     list-style-type: none;
+    margin-top: 2em;
 `;
 
 const Name = styled.p`
@@ -16,6 +21,7 @@ const Name = styled.p`
     color: white;
     border-radius: 2px;
     text-decoration: none;
+    margin: 0;
 
     :hover {
         cursor: default;
@@ -23,9 +29,11 @@ const Name = styled.p`
 `;
 
 const DifficultyName = styled(Name)`
+    margin: 0;
+
     :hover {
-      background-color: #a36b6c;
-      cursor: pointer;
+        background-color: #a36b6c;
+        cursor: pointer;
     }
 `;
 
@@ -34,11 +42,28 @@ const ListItem = styled.li`
 `;
 
 const Title = styled(ListItem)`
-    margin-right: auto;
+    margin: 0 auto;
+    @media (min-width: 525px) {
+        margin-left: 0;
+        margin-right: auto;
+    }
 `;
 
 const Button = styled(ListItem)`
-    margin-left: 1.5em;
+    margin-left: ${props => (props.left ? 'auto' : '0.75em')};
+    margin-right: ${props => (props.right ? 'auto' : '0.75em')};
+    @media (min-width: 525px) {
+        margin-left: 1.5em;
+        margin-right: 0;
+    }
+`;
+
+const Difficulties = styled.div`
+    display: flex;
+    margin-top: 1em;
+    @media (min-width: 525px) {
+        margin-top: 0;
+    }
 `;
 
 const Navigation = ({
@@ -47,9 +72,11 @@ const Navigation = ({
   <nav>
     <Items>
       <Title><Name>Graph Isomorphism</Name></Title>
-      <Button onClick={onEasy(uri)}><DifficultyName chosen={difficulty === EASY}>Easy</DifficultyName></Button>
-      <Button onClick={onMedium(uri)}><DifficultyName chosen={difficulty === MEDIUM}>Medium</DifficultyName></Button>
-      <Button onClick={onHard(uri)}><DifficultyName chosen={difficulty === HARD}>Hard</DifficultyName></Button>
+      <Difficulties>
+        <Button left onClick={onEasy(uri)}><DifficultyName chosen={difficulty === EASY}>Easy</DifficultyName></Button>
+        <Button onClick={onMedium(uri)}><DifficultyName chosen={difficulty === MEDIUM}>Medium</DifficultyName></Button>
+        <Button right onClick={onHard(uri)}><DifficultyName chosen={difficulty === HARD}>Hard</DifficultyName></Button>
+      </Difficulties>
     </Items>
   </nav>
 );
